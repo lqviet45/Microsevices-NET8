acd636b7d16fcf81b9a00657c7b1c8b7d062a126
lqviet45 <lqviet455@gmail.com>
2024-01-10 15:09:19 +0700

    End of section 2

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public virtual DbSet<Auction> Auctions { get; set; }
+    public virtual DbSet<Item> Items { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
