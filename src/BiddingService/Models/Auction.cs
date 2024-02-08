79e1d07d9584eaa1efb064f62569b2089261387a
lqviet45 <lqviet455@gmail.com>
2024-02-08 22:23:12 +0700

    Add BiddingService project to solution

---
+++ src/BiddingService/Models/Auction.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Entities;
+
+namespace BiddingService;
+
+public class Auction : Entity
+{
+    public DateTime AuctionEnd { get; set; }
+    public string Seller { get; set; }
+    
+}
